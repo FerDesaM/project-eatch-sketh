9a8e31d225da53e39b371da41532c4163cc3f936
FerDesaM <fdezaso@unsa.edu.pe>
2024-03-13 19:05:32 -0500

    First changes of the code add hover for the grid

--- script.js
+++ script.js
@@ -2,22 +2,41 @@ const container=document.querySelector("#container");
 function createGrid(){
     const grid=document.createElement("div");
     grid.classList.add("grid");
-    grid.setAttribute("style","width:64px; height:64px; border: 0.2px solid black;")
     return grid;
 }
-function Grid(rows,cols){
-    for(let i=0;i<rows;i++){
-        for(let j=0;j<cols;j++){
+function Grid(numberdivs){
+    let number_of_divs=numberdivs;
+    let create_divs=number_of_divs**2;
+    for(let i=0;i<create_divs;i++){
             const grid=createGrid();
             container.appendChild(grid);
+    }
+    const divs=document.querySelectorAll("#container div");
+    divs.forEach(
+        function(div) {
+            div.style.width="calc(100% / " + number_of_divs + ")";
         }
+    )
+}
+
+Grid(16);
+
+
+function callRowsCols(){
+    const divs = parseInt(prompt("Number of divs"));
+    if (divs > 100) {
+        alert("You have exceeded the limit");
+    } else if (divs <= 0 || isNaN(divs)) {
+        alert("Invalid input. Please enter a valid number.");
+    } else {
+        Grid(divs);
     }
 }
-Grid(16,16);
-function callRowsCols(){
-    const rows=parseInt(prompt("INGRESE FILAS"));
-    const cols=parseInt(prompt("INGRESE COLUMNAS"));
-    Grid(rows,cols);    
-}
+ 
+
 const select=document.querySelector("#select")
 select.addEventListener("click",callRowsCols)
+
+function AddCollor(){
+    
+}
